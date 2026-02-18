eb460c333a24ddbbb6efefcdca6d353395bb617b
vinodh1988 <k.c.vinodh@hotmail.com>
2026-02-18 14:21:25 +0530

    code

---
+++ Scripts/input.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter your name:"
+read name
+
+echo "Enter your city:"
+read city
+
+echo "Enter your age:"
+read age
+
+echo ""
+echo "Name: $name"
+echo "City: $city"
+echo "Age: $age"
